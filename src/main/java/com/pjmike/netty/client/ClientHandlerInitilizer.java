34034498ed4657a29dde23ff6f4cdabbe35a5e22
zhaoxin <15237139190>
2022-03-23 15:11:28 +0800

    fix bugs

--- src/main/java/com/pjmike/netty/client/ClientHandlerInitilizer.java
+++ src/main/java/com/pjmike/netty/client/ClientHandlerInitilizer.java
@@ -8,12 +8,22 @@ import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.timeout.IdleStateHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author pjmike
  * @create 2018-10-24 16:35
  */
 public class ClientHandlerInitilizer extends ChannelInitializer<Channel> {
+
+    @Autowired
+    HeartbeatHandler heartbeatHandler;
+
+    NettyClient nettyClient;
+
+    public ClientHandlerInitilizer(NettyClient nettyClient) {
+        this.nettyClient = nettyClient;
+    }
 
     @Override
     protected void initChannel(Channel ch) throws Exception {
@@ -23,7 +33,7 @@ public class ClientHandlerInitilizer extends ChannelInitializer<Channel> {
                 .addLast(new ProtobufDecoder(MessageBase.Message.getDefaultInstance()))
                 .addLast(new ProtobufVarint32LengthFieldPrepender())
                 .addLast(new ProtobufEncoder())
-                .addLast(new HeartbeatHandler())
+                .addLast(new HeartbeatHandler(nettyClient))
                 .addLast(new NettyClientHandler());
     }
 }
